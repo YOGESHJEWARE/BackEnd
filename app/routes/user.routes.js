38df6f32349e746232a83047ef5f8d4cc5d1254c
YOGESHJEWARE <86521765+YOGESHJEWARE@users.noreply.github.com>
2022-01-24 13:51:28 +0530

    Add files via upload

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -11,6 +11,8 @@ module.exports= app =>{
 
     router.get('/user/:id',users.getSingleUser)
     
+    // router.put('/editUser/:id',users.updateUser)
+    
     app.use('/api',router)  //context path
  
 
